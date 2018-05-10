6819760df0b1f35b54660acc7c36ab4825a5f159
Jared Hocutt <jaredhocutt@gmail.com>
2018-05-10 08:44:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# OpenShift Provisioner
+
+This project is a work in progress.
+
+## AWS
+
+Required variables:
+  - cluster_name
+  - aws_region
+  - route53_hosted_zone
+  - route53_hosted_zone_id
+  - ec2_key_name
+  - ec2_key_file
+  - rhsm_username
+  - rhsm_password
+  - rhsm_pool
+
+Recommended variables:
+  - ec2_ami_type
+  - openshift_version
+
+Required environment variables:
+  - AWS_ACCESS_KEY_ID
+  - AWS_SECRET_ACCESS_KEY
---
+++ playbooks/roles/provision_aws/defaults/rhel_ami.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ec2_regions=$(aws ec2 describe-regions --query 'Regions[].{Name:RegionName}' --output text)
+
+echo "ec2_ami_ids:"
+
+for ec2_region in ${ec2_regions};
+do
+  echo "  ${ec2_region}:"
+
+  access_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.4?*GA*Access*" --region ${ec2_region} --output text | sort -r))
+  echo "    cloud_access: ${access_images[0]}"
+  hourly_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.4?*GA*Hourly*" --region ${ec2_region} --output text | sort -r))
+  echo "    hourly: ${access_images[0]}"
+done
