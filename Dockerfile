f653dacb2cb2f71f143e8bf424488d5635869f63
Jared Hocutt <jaredhocutt@gmail.com>
2018-05-30 23:15:33 -0400

    Create Dockerfile and entrypoint.sh

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM docker.io/library/fedora:27
+
+RUN \
+    dnf install -y \
+        openssh-clients \
+        python3 \
+        python3-pip \
+        python3-devel \
+        which \
+    && ln -sf /usr/bin/python3 /usr/local/bin/python \
+    && ln -sf /usr/bin/pip3 /usr/local/bin/pip \
+    && pip install --upgrade pip \
+    && pip install pipenv \
+    && dnf clean all
+
+COPY . /app
+WORKDIR /app
+
+RUN pipenv install --system --deploy
+
+VOLUME /app
+ENTRYPOINT ["/app/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+USER_UID=$(id --user)
+
+if [[ $USER_UID == 0 ]]; then
+  echo "Do not run this container as root."
+  echo
+  echo "It is recommended to pass the following option when starting the container:"
+  echo "    --user \$(id -u \$USER)"
+  echo
+  echo "Example:"
+  echo "    docker run -it --rm --name openshift-provision --volume \$(pwd):/app:z openshift-provision"
+
+  exit 1
+fi
+
+useradd --uid $(id --user) --gid $(id --group) --shell $(which bash) --home-dir /app --no-create-home provision
+
+if [[ $# == 0 ]]; then
+  if [[ -t 0 ]]; then
+    echo "Starting shell..."
+    echo
+
+    exec "bash"
+  else
+    echo "An interactive shell was not detected."
+    echo
+    echo "By default, this container starts a bash shell, be sure you are passing '-it' to your run command."
+
+    exit 1
+  fi
+else
+  exec "$@"
+fi
