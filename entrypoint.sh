aec5102386c7950fa932ce2705444a6212ae5e5e
Jared Hocutt <jaredhocutt@gmail.com>
2018-06-21 08:49:16 -0400

    Let container run as root and fix permissions on key pairs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,6 @@
 
 set -e
 
-USER_UID=$(id --user)
-
-if [[ $USER_UID == 0 ]]; then
-  echo "Do not run this container as root."
-  echo
-  echo "It is recommended to pass the following option when starting the container:"
-  echo "    --user \$(id -u \$USER)"
-  echo
-  echo "Example:"
-  echo "    podman run -it --rm --user $(id -u $USER) --volume $(pwd):/app:z --env-file vars/aws.env openshift-provision"
-
-  exit 1
-fi
-
-useradd --uid $(id --user) --gid $(id --group) --shell $(which bash) --home-dir /app --no-create-home provision
-
 if [[ $# == 0 ]]; then
   if [[ -t 0 ]]; then
     echo "Starting shell..."
