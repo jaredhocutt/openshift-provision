1c5f64928d8a35a6a2ce18822c9ff589ae6dd308
Jared Hocutt <jhocutt@redhat.com>
2018-08-13 17:00:09 -0400

    Update AWS EC2 plugin config

--- playbooks/roles/provision_aws/defaults/rhel_ami.sh
+++ playbooks/roles/provision_aws/defaults/rhel_ami.sh
@@ -8,8 +8,8 @@ for ec2_region in ${ec2_regions};
 do
   echo "  ${ec2_region}:"
 
-  access_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.4?*GA*Access*" --region ${ec2_region} --output text | sort -r))
+  access_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.5?*GA*Access*" --region ${ec2_region} --output text | sort -r))
   echo "    cloud_access: ${access_images[0]}"
-  hourly_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.4?*GA*Hourly*" --region ${ec2_region} --output text | sort -r))
+  hourly_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.5?*GA*Hourly*" --region ${ec2_region} --output text | sort -r))
   echo "    hourly: ${access_images[0]}"
 done
