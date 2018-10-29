a9c9e7426b1c68155ba7082acc25e4c40299e303
Jared Hocutt <jhocutt@redhat.com>
2018-10-29 17:53:48 -0400

    Update default images to RHEL 7.5

--- playbooks/roles/provision_aws/defaults/rhel_ami.sh
+++ playbooks/roles/provision_aws/defaults/rhel_ami.sh
@@ -11,5 +11,5 @@ do
   access_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.5?*GA*Access*" --region ${ec2_region} --output text | sort -r))
   echo "    cloud_access: ${access_images[0]}"
   hourly_images=($(aws ec2 describe-images --owners 309956199498 --query 'Images[*].[ImageId]' --filters "Name=name,Values=RHEL-7.5?*GA*Hourly*" --region ${ec2_region} --output text | sort -r))
-  echo "    hourly: ${access_images[0]}"
+  echo "    hourly: ${hourly_images[0]}"
 done
